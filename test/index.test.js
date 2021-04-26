4b29e93aea3c172db7ea210e324d697448c0720f
ed kim <ekim8015@gmail.com>
2021-04-26 19:52:38 +1000

    add a test!
    Signed-off-by: ed kim <ekim8015@gmail.com>

--- test/index.test.js
+++ test/index.test.js
@@ -42,4 +42,25 @@ describe('Token_Generation_Test', function () {
             'LVNpZ25hdHVyZT1mZjFjODljMTZmMjUyNzVmOGE0YzczMTE2NzhmNDQ3NzIy' +
             'NjQ2ZDY0MzU4ODg1NGExMTRkMTY4YjA2MmM0YmYw');
     });
+
+    it('renew_token_with_sessionToken', async function () {
+        EKSToken.config = {
+            accessKeyId: 'AKID',
+            secretAccessKey: 'SECRET',
+            sessionToken: 'FwoGZXIvYXdzEMP//////////wEaDLuLD7I1gA6LW1mZcSKxAZWj_trunc',
+            region: 'us-west-2'
+        };
+        let token = await EKSToken.renew('eks-cluster', '60', '20200930T093726Z');
+        expect(token).to.be.equal('k8s-aws-v1.' +
+            'aHR0cHM6Ly9zdHMudXMtd2VzdC0yLmFtYXpvbmF3cy5jb20vP0FjdGlvbj1H' +
+            'ZXRDYWxsZXJJZGVudGl0eSZWZXJzaW9uPTIwMTEtMDYtMTUmWC1BbXotQWxn' +
+            'b3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lE' +
+            'JTJGMjAyMDA5MzAlMkZ1cy13ZXN0LTIlMkZzdHMlMkZhd3M0X3JlcXVlc3Qm' +
+            'WC1BbXotRGF0ZT0yMDIwMDkzMFQwOTM3MjZaJlgtQW16LUV4cGlyZXM9NjAm' +
+            'WC1BbXotU2VjdXJpdHktVG9rZW49RndvR1pYSXZZWGR6RU1QJTJGJTJGJTJG' +
+            'JTJGJTJGJTJGJTJGJTJGJTJGJTJGd0VhREx1TEQ3STFnQTZMVzFtWmNTS3hB' +
+            'WldqX3RydW5jJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCUzQngtazhzLWF3' +
+            'cy1pZCZYLUFtei1TaWduYXR1cmU9OWQzZDU5ZmVlNjk5NDUwMjI0YjcwOTli' +
+            'YjZkY2U1YmNhMGE3Njc3ODcwMjMyYTNjMTczMDE2MTNmMDAxMjYxNQ');
+    });
 });
